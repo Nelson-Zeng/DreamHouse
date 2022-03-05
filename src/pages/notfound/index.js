9b7df05736d3a8a12c6bdf1f6c0402208406914d
nelsonzeng <zhichao.zeng@nx-engine.com>
2022-03-05 22:14:11 +0800

    feat: 缺省页

--- src/pages/notfound/index.js
+++ src/pages/notfound/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './index.css';
+import Avatar from './404.png';
 
 const NotFound = () => (
-  <div>未知页面</div>
+  <div className='not-found'>
+    <div className='not-found__container'>
+      <img src={Avatar}/>
+      <div className='not-found__text'>当前页面不存在</div>
+    </div>
+  </div>
 );
 
 export default NotFound;
